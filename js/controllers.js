42553048cf63f519b77b69b340da76e9f484d926
hdillon <dillonhoraciodavid@gmail.com>
2016-10-25 20:04:52 -0300

    Fix template error

--- js/controllers.js
+++ js/controllers.js
@@ -3,4 +3,7 @@ angular.module('TPInmobiliaria.controllers', [])
 
 app.controller('controlInicio', function($scope, $http) {
   $scope.titulo="Inicio y presentacion de la WEB";  
+
+  //console.log($(".navbar").offset().top);
+
 });
