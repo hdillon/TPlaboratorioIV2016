55857220da4cfca008aa5b430f85748f9b2b1cdb
hdillon <dillonhoraciodavid@gmail.com>
2016-10-25 21:04:00 -0300

    States Usuario

--- js/app.js
+++ js/app.js
@@ -7,7 +7,31 @@ app.config(function($stateProvider, $urlRouterProvider) {
       .state('inicio', {
                 url : '/inicio',
                 templateUrl : 'vistas/inicio.html',
-                controller : 'controlInicio'
+                controller : 'ControlInicio'
+            })
+      .state('usuario', {
+                url : '/usuario',
+                abstract:true,
+                templateUrl : 'vistas/abstractaUsuario.html',
+                controller : 'ControlUsuario'
+            })
+      .state('usuario.login', {
+                url: '/login',
+                views: {
+                    'contenido': {
+                        templateUrl: 'vistas/usuarioLogin.html',
+                        controller : 'ControlUsuario'
+                    }
+                }
+            })
+      .state('usuario.registrarse', {
+                url: '/registrarse',
+                views: {
+                    'contenido': {
+                        templateUrl: 'vistas/usuarioRegistrarse.html',
+                        controller : 'ControlUsuario'
+                    }
+                }
             })
       
              $urlRouterProvider.otherwise('/inicio');
--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,16 @@
 angular.module('TPInmobiliaria.controllers', [])
 
 
-app.controller('controlInicio', function($scope, $http) {
-  $scope.titulo="Inicio y presentacion de la WEB";  
-
+app.controller('ControlInicio', function($scope, $http, $state) {
   //console.log($(".navbar").offset().top);
+  $scope.Login = function(){
+    $state.go("usuario.login");
+  }
+
+})
+
+app.controller('ControlUsuario', function($scope, $http, $state) {
 
 });
+
+
